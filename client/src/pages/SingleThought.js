16996ddef231520b9c5333a6bafd1f4e4ad8c35d
Jonathan <eimerarts@gmail.com>
2020-10-20 19:42:13 -0500

    hopefully final

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -4,6 +4,7 @@ import { useQuery } from '@apollo/react-hooks';
 import { QUERY_THOUGHT } from '../utils/queries';
 import ReactionList from '../components/ReactionList';
 import ReactionForm from '../components/ReactionForm';
+import Auth from '../utils/auth';
 
 const SingleThought = props => {
 
